665929d47bf09ef6a3bf17ccd83b7071b5a55162
Anna Mazur <amfolity@gmail.com>
2020-09-02 21:12:13 +0000

    lint improvements

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,16 +1,17 @@
-import numpy as np
-import pyflann
 import random
 from collections import namedtuple
+import numpy as np
+import pyflann
 
 Transition = namedtuple('Transition',
                         ('state', 'action', 'reward', 'next_state'))
 
-class ReplayBuffer(object):
+class ReplayBuffer():
     """ 
     Cyclic buffer that stores transitions and embeddings observed. 
     Additionally supports  functionality to find and return fixed number of neigbors for each index. 
-    Search for nearest neigbors is done with help of FLANN library for fast approximate nearest neighbors search 
+    Search for nearest neigbors is done with help of FLANN library 
+    for fast approximate nearest neighbors search 
     """
     
     def __init__(self, capacity, emb_dimension, path_length, rebuild_freq=500):
@@ -25,7 +26,8 @@ class ReplayBuffer(object):
         self.last_rebuild_position = 0
 
     def push(self, state, action, reward, next_state, embedding):
-        """ Saves a transition (state, action, reward, next_state) and an embedding in cyclic arrays """
+        """ Saves a transition (state, action, reward, next_state) and
+            an embedding in cyclic arrays """
         
         self.rebuild_counter+=1
         if len(self.memory) < self.capacity:
@@ -33,13 +35,12 @@ class ReplayBuffer(object):
         else:
             if self.rebuild_counter >= self.rebuild_freq:
                 self.rebuild_counter = 0
-                self.engine.build_index(self.embedding)
+                self.engine.build_index(self.embeddings)
                 self.last_rebuild_position = self.position
 
         self.memory[self.position]      = Transition(state, action, reward, next_state)
         self.embeddings[self.position]  = embedding
         self.position                   = (self.position + 1) % self.capacity
-
 
     def _is_idx_permitted(self, idx):
         """ 
@@ -52,25 +53,25 @@ class ReplayBuffer(object):
            have enough steps of experience yet, for the case it is very recent idx,
            yet the engine was already rebuilt with its embedding.
         """
-           
+
         # Not permitted if we have already rewritten an old entry in experience replay
-        # but haven't rebuilt the nn_index yet, so it still contains an old embedding. 
-        if (self.position >= self.last_rebuild_position):
-            if(idx <=self.position and idx >= self.last_rebuild_position):
+        # but haven't rebuilt the nn_index yet, so it still contains an old embedding.
+        if self.position >= self.last_rebuild_position:
+            if (idx <=self.position) and (idx >= self.last_rebuild_position):
                 return False
 
         # Same as above, but for the case that we cycled through the memory recently
         # so the current position is in the beginning of the array,
         # while last_rebuild_position points to somewhere near its end.
         else:
-            if (idx <=self.position or idx >= self.last_rebuild_position):
+            if (idx <=self.position) or (idx >= self.last_rebuild_position):
                 return False
         
         # Not permitted if we cannot build full trajectory from this point,
         # because it is the most recent experience and we haven't played it for
         # self.path_length ahead yet.
-        if (self.position >= self.path_length):
-            if(idx <=self.position and idx >= self.position - self.path_length):
+        if self.position >= self.path_length:
+            if (idx <= self.position) and (idx >= (self.position - self.path_length)):
                 return False
 
         # Same as above, but for the case that we cycled through the memory recently
@@ -95,9 +96,8 @@ class ReplayBuffer(object):
         # And return the nearest num_neighbors from the rest
         idxs       = [idx for idx in idxs if self._is_idx_permitted(idx)][:num_neighbors]
         if return_embeddings:
-            return idxs, [self.embedding[idx] for idx in idxs]
-        else:
-            return idxs
+            return idxs, [self.embeddings[idx] for idx in idxs]
+        return idxs
 
     def sample(self, batch_size):
         """ Sample batch of transitions (state, action, reward, next_state) """
